c79e1963ca2d2cc9c995f05a959e563509b95095
Lewis Booth <lewisbooth@Lewiss-MacBook-Pro.local>
2017-02-21 18:33:04 +0000

    Created components and began wiring together

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes'
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+        this.addToOrder = this.addToOrder.bind(this);
+
+        // Initial State
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+    }
+    addFish(fish) {
+        const fishes = {...this.state.fishes};
+        const timeStamp = Date.now();
+        fishes[`fish-${timeStamp}`] = fish;
+        this.setState({ fishes });
+    }
+    loadSamples() {
+        this.setState({
+            fishes: sampleFishes
+        })
+    }
+    addToOrder(key) {
+        const order = {...this.state.order};
+        order[key] = order[key] + 1 || 1;
+        this.setState({ order })
+    }
+    render() {
+        return (
+        <div className='catch-of-the-day'>
+            <div className='menu'>
+                <Header tagline="Fresh Seafood Market" />
+                <ul className='list-of-fishes'>
+                    {
+                      Object
+                        .keys(this.state.fishes)
+                        .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
+                    }
+                </ul>
+            </div>
+            <Order />
+            <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+        </div>
+        )
+    }
+}
+
+export default App;
---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { getFunName } from '../helpers'
+
+class StorePicker extends React.Component {
+    goToStore(event) {
+        event.preventDefault();
+        this.context.router.transitionTo(`/store/${this.storeInput.value}`)
+    }
+    render() {
+        return (
+        <form className='store-selector' onSubmit={(e) => this.goToStore(e)}>
+            <h2>Please Enter a Store</h2>
+            <input type='text' required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
+            <button type="submit">Visit Store</button>
+        </form>
+        )
+    }
+}
+
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
+} 
+
+export default StorePicker;
