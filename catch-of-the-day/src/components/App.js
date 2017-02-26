899ae6db155b490a1a3d4c976cdd2b29c437555a
Lewis Booth <lewis@lewisbooth.co.uk>
2017-02-26 16:19:22 +0000

    Fully functional without auth

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -13,6 +13,9 @@ class App extends React.Component {
         this.addFish = this.addFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
+        this.updateFish = this.updateFish.bind(this);
+        this.removeFish = this.removeFish.bind(this);
+        this.removeFishOrder = this.removeFishOrder.bind(this);
 
         // Initial State
         this.state = {
@@ -46,6 +49,21 @@ class App extends React.Component {
         fishes[`fish-${timeStamp}`] = fish;
         this.setState({ fishes });
     }
+    updateFish(key, updatedFish) {
+        const fishes = {...this.state.fishes};
+        fishes[key] = updatedFish;
+        this.setState({ fishes });
+    }
+    removeFish(key) {
+        const fishes = {...this.state.fishes};
+        fishes[key] = null;
+        this.setState({ fishes });
+    }
+    removeFishOrder(key) {
+        const order = {...this.state.order};
+        delete order[key];
+        this.setState({ order });
+    }
     loadSamples() {
         this.setState({
             fishes: sampleFishes
@@ -73,8 +91,15 @@ class App extends React.Component {
                 fishes={this.state.fishes} 
                 order={this.state.order}
                 params={this.props.params}
+                removeFishOrder={this.removeFishOrder}
             />
-            <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+            <Inventory 
+                addFish={this.addFish} 
+                loadSamples={this.loadSamples} 
+                fishes={this.state.fishes} 
+                updateFish={this.updateFish}
+                removeFish={this.removeFish}
+            />
         </div>
         )
     }
